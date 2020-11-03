1579bd86d4d84816c2b992b54a9e5d6d30fe998d
Rayarise <rayarise23@gmail.com>
2020-11-03 11:41:21 -0500

    T01 CelsisInFahrenheit_AlwaysReturnGoodValue : Done

--- WeatherApp/ViewModels/TemperatureViewModel.cs
+++ WeatherApp/ViewModels/TemperatureViewModel.cs
@@ -7,5 +7,11 @@ namespace WeatherApp.ViewModels
     public class TemperatureViewModel : BaseViewModel
     {
         /// TODO : Ajoutez le code nécessaire pour réussir les tests et répondre aux requis du projet
+        /// 
+        public static double CelsiusInFahrenheit(double c) {
+
+            return Math.Round(((c * 9) / 5) + 32, 1);  
+            
+        }
     }
 }
